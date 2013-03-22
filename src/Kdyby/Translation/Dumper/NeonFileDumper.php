e6897526aba417a737e0d3537034f4fbd38ae8c5
Filip Procházka <hosiplan@gmail.com>
2013-03-22 12:18:11 +0100

    Support neon

---
+++ src/Kdyby/Translation/Dumper/NeonFileDumper.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * This file is part of the Kdyby (http://www.kdyby.org)
+ *
+ * Copyright (c) 2008 Filip Procházka (filip@prochazka.su)
+ *
+ * For the full copyright and license information, please view the file license.txt that was distributed with this source code.
+ */
+
+namespace Kdyby\Translation\Dumper;
+
+use Nette\Utils\Neon;
+use Symfony\Component\Translation\Dumper\FileDumper;
+use Symfony\Component\Translation\MessageCatalogue;
+
+
+
+/**
+ * Generates Neon files from a message catalogue.
+ *
+ * @author Filip Procházka <filip@prochazka.su>
+ */
+class NeonFileDumper extends FileDumper
+{
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function format(MessageCatalogue $messages, $domain)
+    {
+         return Neon::encode($messages->all($domain), Neon::BLOCK);
+    }
+
+
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function getExtension()
+    {
+        return 'neon';
+    }
+
+}
---
+++ src/Kdyby/Translation/Loader/NeonFileLoader.php
@@ -0,0 +1,65 @@
+<?php
+
+/**
+ * This file is part of the Kdyby (http://www.kdyby.org)
+ *
+ * Copyright (c) 2008 Filip Procházka (filip@prochazka.su)
+ *
+ * For the full copyright and license information, please view the file license.txt that was distributed with this source code.
+ */
+
+namespace Kdyby\Translation\Dumper;
+
+use Nette\Utils\Neon;
+use Nette\Utils\NeonException;
+use Symfony\Component\Translation\Exception\InvalidResourceException;
+use Symfony\Component\Translation\Exception\NotFoundResourceException;
+use Symfony\Component\Config\Resource\FileResource;
+use Symfony\Component\Translation\Loader\ArrayLoader;
+use Symfony\Component\Translation\Loader\LoaderInterface;
+
+
+
+/**
+ * Loads translations from Neon files.
+ *
+ * @author Filip Procházka <filip@prochazka.su>
+ */
+class NeonFileLoader extends ArrayLoader implements LoaderInterface
+{
+
+    /**
+     * {@inheritdoc}
+     */
+    public function load($resource, $locale, $domain = 'messages')
+    {
+        if (!stream_is_local($resource)) {
+            throw new InvalidResourceException(sprintf('This is not a local file "%s".', $resource));
+        }
+
+        if (!file_exists($resource)) {
+            throw new NotFoundResourceException(sprintf('File "%s" not found.', $resource));
+        }
+
+        try {
+            $messages = Neon::decode($resource);
+
+        } catch (NeonException $e) {
+            throw new InvalidResourceException('Error parsing Neon.', 0, $e);
+        }
+
+        if (empty($messages)) {
+            $messages = array();
+        }
+
+        if (!is_array($messages)) {
+            throw new InvalidResourceException(sprintf('The file "%s" must contain a Neon array.', $resource));
+        }
+
+        $catalogue = parent::load($messages, $locale, $domain);
+        $catalogue->addResource(new FileResource($resource));
+
+        return $catalogue;
+    }
+
+}
