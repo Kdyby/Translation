84fdfaf512a6471a31993d5d207557d7bab0f679
Filip Procházka <filip@prochazka.su>
2013-06-30 15:09:44 +0200

    More robust fix for Nette compatibility

--- src/Kdyby/Translation/DI/TranslationExtension.php
+++ src/Kdyby/Translation/DI/TranslationExtension.php
@@ -24,9 +24,12 @@ use Symfony\Component\Translation\Loader\LoaderInterface;
 
 if (!class_exists('Nette\DI\CompilerExtension')) {
 	class_alias('Nette\Config\CompilerExtension', 'Nette\DI\CompilerExtension');
-	class_alias('Nette\Config\Configurator', 'Nette\Configurator');
 	class_alias('Nette\Config\Compiler', 'Nette\DI\Compiler');
 	class_alias('Nette\Config\Helpers', 'Nette\DI\Config\Helpers');
+}
+
+if (!class_exists('Nette\Configurator')) {
+	class_alias('Nette\Config\Configurator', 'Nette\Configurator');
 }
 
 /**
