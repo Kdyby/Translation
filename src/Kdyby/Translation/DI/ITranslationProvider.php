3b7213a45fb4d74cbc5c8504f8c9e6e4d2b8838e
Filip Procházka <filip@prochazka.su>
2013-11-02 18:22:47 +0100

    Added ITranslationProvider for translation resources

---
+++ src/Kdyby/Translation/DI/ITranslationProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * This file is part of the Kdyby (http://www.kdyby.org)
+ *
+ * Copyright (c) 2008 Filip Procházka (filip@prochazka.su)
+ *
+ * For the full copyright and license information, please view the file license.txt that was distributed with this source code.
+ */
+
+namespace Kdyby\Translation\DI;
+
+use Kdyby;
+use Nette;
+
+
+
+/**
+ * @author Filip Procházka <filip@prochazka.su>
+ */
+interface ITranslationProvider
+{
+
+	/**
+	 * Return array of directories, that contain resources for translator.
+	 *
+	 * @return string[]
+	 */
+	function getTranslationResources();
+
+}
