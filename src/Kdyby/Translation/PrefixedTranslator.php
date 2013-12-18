b0648346cd335a983e7eb41d720c1f274572dfbd
Filip Procházka <filip@prochazka.su>
2013-12-18 23:15:19 +0100

    Implemented translator domain macro for easier writing of templates

---
+++ src/Kdyby/Translation/PrefixedTranslator.php
@@ -0,0 +1,137 @@
+<?php
+
+/**
+ * This file is part of the Kdyby (http://www.kdyby.org)
+ *
+ * Copyright (c) 2008 Filip Procházka (filip@prochazka.su)
+ *
+ * For the full copyright and license information, please view the file license.txt that was distributed with this source code.
+ */
+
+namespace Kdyby\Translation;
+
+use Kdyby;
+use Nette;
+use Nette\Localization\ITranslator;
+use Nette\Utils\Callback;
+
+
+
+/**
+ * @author Filip Procházka <filip@prochazka.su>
+ */
+class PrefixedTranslator extends Nette\Object implements ITranslator
+{
+
+	/**
+	 * @var \Nette\Localization\ITranslator
+	 */
+	private $translator;
+
+	/**
+	 * @var string
+	 */
+	private $prefix;
+
+
+
+	public function __construct($prefix, ITranslator $translator)
+	{
+		if ($translator instanceof PrefixedTranslator) { // todo: this is just an experiment
+			$translator = $translator->unwrap();
+		}
+
+		$this->translator = $translator;
+		$this->prefix = rtrim($prefix, '.');
+	}
+
+
+
+	public function translate($message, $count = NULL, $parameters = array(), $domain = NULL, $locale = NULL)
+	{
+		if (is_array($count)) {
+			$locale = $domain ? : NULL;
+			$domain = $parameters ? : NULL;
+			$parameters = $count ? : array();
+			$count = NULL;
+		}
+
+		return $this->translator->translate($this->prefix . '.' . $message, $count, (array) $parameters, $domain, $locale);
+	}
+
+
+
+	/**
+	 * @return ITranslator
+	 */
+	public function unwrap()
+	{
+		return $this->translator;
+	}
+
+
+
+	/**
+	 * @param Nette\Templating\Template $template
+	 * @return ITranslator
+	 */
+	public function unregister(Nette\Templating\Template $template)
+	{
+		$translator = $this->unwrap();
+		$template->registerHelper('translator', array(new TemplateHelpers($translator), 'translate'));
+		return $translator;
+	}
+
+
+
+	/**
+	 * @param Nette\Templating\Template $template
+	 * @param string $prefix
+	 * @return ITranslator
+	 * @throws InvalidArgumentException
+	 */
+	public static function register(Nette\Templating\Template $template, $prefix)
+	{
+		if (!$translator = self::findTranslator($template)) {
+			throw new InvalidArgumentException("You have to pass the Translator the the template using `\$template->setTranslator(\$translator);`, or register the helper loader.");
+		}
+
+		/** @var ITranslator $translator */
+		$translator = new static($prefix, $translator);
+		$template->setTranslator($translator);
+
+		return $translator;
+	}
+
+
+
+	/**
+	 * @param Nette\Templating\Template $template
+	 * @return ITranslator
+	 */
+	private static function findTranslator(Nette\Templating\Template $template)
+	{
+		$helpers = $template->getHelpers();
+		if (isset($helpers['translate'])) {
+			$obj = isset($helpers['translate'][0]) ? $helpers['translate'][0] : NULL;
+			if ($obj instanceof ITranslator) {
+				return $obj;
+
+			} elseif ($obj instanceof TemplateHelpers) {
+				return $obj->getTranslator();
+			}
+		}
+
+		foreach ($template->getHelperLoaders() as $loader) {
+			$obj = is_array($loader) && isset($loader[0]) ? $loader[0] : NULL;
+			if ($obj instanceof TemplateHelpers) {
+				$template->setTranslator($translator = $obj->getTranslator());
+				return $translator;
+			}
+		}
+
+		return NULL;
+	}
+
+}
+
--- tests/KdybyTests/Translation/TranslateMacros.phpt
+++ tests/KdybyTests/Translation/TranslateMacros.phpt
@@ -25,20 +25,9 @@ require_once __DIR__ . '/../bootstrap.php';
 class TranslateMacrosTest extends TestCase
 {
 
-	public function testRender()
+	public function testRender_translate()
 	{
-		$container = $this->createContainer();
-
-		/** @var Kdyby\Translation\Translator $translator */
-		$translator = $container->getByType('Nette\Localization\ITranslator');
-		$translator->setFallbackLocales(array('cs_CZ', 'cs'));
-		$translator->setLocale('cs');
-
-		$template = new Nette\Templating\FileTemplate(__DIR__ . '/files/Homepage.default.latte');
-		$template->setParameters(array('translator' => $translator));
-		$template->registerHelperLoader(array($translator->createTemplateHelpers(), 'loader'));
-		$template->registerFilter($engine = new Nette\Latte\Engine());
-		Kdyby\Translation\Latte\TranslateMacros::install($engine->getCompiler());
+		$template = $this->buildTemplate()->setFile(__DIR__ . '/files/Homepage.default.latte');
 
 		Assert::same('Ahoj %name%
 Ahoj Peter
@@ -59,6 +48,62 @@ Hello Peter|Helloes Peter
 missingKey.namedHelloCounting
 missingKey.namedHelloCounting
 missingKey.namedHelloCounting' . "\n", $template->__toString());
+	}
+
+
+
+	public function testRender_translate_prefixed()
+	{
+		$template = $this->buildTemplate()->setFile(__DIR__ . '/files/Order.default.latte');
+
+		Assert::same('
+Ahoj %name%
+Ahoj Peter
+Ahoj Peter
+
+Ahoj %name%
+Ahoj Peter
+Ahoj Peter
+
+
+
+missingKey.namedHello
+missingKey.namedHello
+missingKey.namedHello
+
+
+
+Helloes %name%
+Helloes Peter
+Hello Peter|Helloes Peter
+
+
+
+missingKey.namedHelloCounting
+missingKey.namedHelloCounting
+missingKey.namedHelloCounting' . "\n", $template->__toString());
+	}
+
+
+
+	/**
+	 * @return Nette\Templating\FileTemplate
+	 */
+	private function buildTemplate()
+	{
+		$container = $this->createContainer();
+
+		/** @var Kdyby\Translation\Translator $translator */
+		$translator = $container->getByType('Nette\Localization\ITranslator');
+		$translator->setFallbackLocales(array('cs_CZ', 'cs'));
+		$translator->setLocale('cs');
+
+		$template = new Nette\Templating\FileTemplate();
+		$template->registerHelperLoader(array($translator->createTemplateHelpers(), 'loader'));
+		$template->registerFilter($engine = new Nette\Latte\Engine());
+		Kdyby\Translation\Latte\TranslateMacros::install($engine->getCompiler());
+
+		return $template;
 	}
 
 }
