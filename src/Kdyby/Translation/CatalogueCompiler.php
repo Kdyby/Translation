7a9156895fdbe734035c33d0fde1d7015d188d6f
Filip Procházka <filip@prochazka.su>
2014-08-03 23:45:46 +0200

    Simplify variable escaping in CatalogueCompiler

--- src/Kdyby/Translation/CatalogueCompiler.php
+++ src/Kdyby/Translation/CatalogueCompiler.php
@@ -127,7 +127,7 @@ class CatalogueCompiler extends Nette\Object
 		$fallbackContent = '';
 		$current = new Code\PhpLiteral('');
 		foreach ($this->fallbackResolver->compute($translator, $locale) as $fallback) {
-			$fallbackSuffix = new Code\PhpLiteral(ucfirst(str_replace('-', '_', Nette\Utils\Strings::webalize($fallback, NULL, FALSE))));
+			$fallbackSuffix = new Code\PhpLiteral(ucfirst(preg_replace('~[^a-z0-9_]~i', '_', $fallback)));
 
 			$fallbackContent .= Code\Helpers::format(<<<EOF
 \$catalogue? = new MessageCatalogue(?, ?);
