a0c8629394dd7ba07a5afd9629c43512df307493
Filip Procházka <filip@prochazka.su>
2014-11-28 16:51:46 +0100

    typo

--- src/Kdyby/Translation/Translator.php
+++ src/Kdyby/Translation/Translator.php
@@ -74,7 +74,6 @@ class Translator extends BaseTranslator implements ITranslator
 	 * @param IUserLocaleResolver $localeResolver
 	 * @param MessageSelector $selector The message selector for pluralization
 	 * @param CatalogueCompiler $catalogueCompiler
-	 * @param CatalogueFactory $catalogueFactory
 	 * @param FallbackResolver $fallbackResolver
 	 * @param IResourceLoader $loader
 	 */
