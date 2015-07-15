2fd0ea5d03bad46a22d59c1d5ef0e892b777bcde
Filip Procházka <filip@prochazka.su>
2015-07-15 03:53:24 +0200

    Compatibility with Symfony 2.5

--- src/Kdyby/Translation/Translator.php
+++ src/Kdyby/Translation/Translator.php
@@ -390,6 +390,21 @@ class Translator extends BaseTranslator implements ITranslator
 
 
 	/**
+	 * Asserts that the locale is valid, throws an Exception if not.
+	 *
+	 * @param string $locale Locale to tests
+	 * @throws \InvalidArgumentException If the locale contains invalid characters
+	 */
+	protected function assertValidLocale($locale)
+	{
+		if (preg_match('~^[a-z0-9@_\\.\\-]*\z~i', $locale) !== 1) {
+			throw new \InvalidArgumentException(sprintf('Invalid "%s" locale.', $locale));
+		}
+	}
+
+
+
+	/**
 	 * @param $message
 	 * @return array
 	 */
