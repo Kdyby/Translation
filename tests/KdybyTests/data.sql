bbb090a0874073254c8eb54a3a72b89271ba4f72
Azathoth <memnarch@seznam.cz>
2015-09-06 00:59:31 +0200

    fixed sql data inicialization.

--- tests/KdybyTests/data.sql
+++ tests/KdybyTests/data.sql
@@ -5,9 +5,9 @@ CREATE TABLE `translation` (
   `updated_at` datetime NOT NULL
 );
 
-INSERT INTO translation2.translation (`key`, locale, message, updated_at) VALUES ('front.header', 'cs', 'záhlaví', '2015-09-06 00:16:49');
-INSERT INTO translation2.translation (`key`, locale, message, updated_at) VALUES ('front.header', 'en', 'header', '2015-08-30 11:28:17');
-INSERT INTO translation2.translation (`key`, locale, message, updated_at) VALUES ('messages.hello', 'cs', 'nazdar', '2015-09-06 00:16:49');
-INSERT INTO translation2.translation (`key`, locale, message, updated_at) VALUES ('messages.world', 'cs', 'svete', '2015-09-06 00:16:49');
-INSERT INTO translation2.translation (`key`, locale, message, updated_at) VALUES ('messages.hello', 'cs', 'hello', '2015-09-06 00:16:49');
-INSERT INTO translation2.translation (`key`, locale, message, updated_at) VALUES ('messages.world', 'cs', 'world', '2015-09-06 00:16:49');
+INSERT INTO translation (`key`, locale, message, updated_at) VALUES ('front.header', 'cs', 'záhlaví', '2015-09-06 00:16:49');
+INSERT INTO translation (`key`, locale, message, updated_at) VALUES ('front.header', 'en', 'header', '2015-08-30 11:28:17');
+INSERT INTO translation (`key`, locale, message, updated_at) VALUES ('messages.hello', 'cs', 'nazdar', '2015-09-06 00:16:49');
+INSERT INTO translation (`key`, locale, message, updated_at) VALUES ('messages.world', 'cs', 'svete', '2015-09-06 00:16:49');
+INSERT INTO translation (`key`, locale, message, updated_at) VALUES ('messages.hello', 'cs', 'hello', '2015-09-06 00:16:49');
+INSERT INTO translation (`key`, locale, message, updated_at) VALUES ('messages.world', 'cs', 'world', '2015-09-06 00:16:49');
