e3ef6f94ebc5e216826dd82f99e7ccbab7e382f3
Azathoth <memnarch@seznam.cz>
2015-09-05 22:25:38 +0200

    Removed commented part of code.

--- src/Kdyby/Translation/DI/TranslationExtension.php
+++ src/Kdyby/Translation/DI/TranslationExtension.php
@@ -308,15 +308,7 @@ class TranslationExtension extends Nette\DI\CompilerExtension
 				->addTag(self::DATABASE_LOADER_TAG, $loader)
 				->addSetup('setTableName', array($config['database']['table']))
 				->addSetup('setColumnNames', array($columns['key'], $columns['locale'], $columns['message'], $columns['updatedAt']));
-/*
-			for future use to modify mapping scheme
-			if ($class->entity === 'Kdyby\Translation\Loader\DoctrineLoader') {
-				$builder->addDefinition($this->prefix('listener'))
-					->setClass('Kdyby\Translation\Listener\TranslationMetadataListener')
-					->addTag(Kdyby\Events\DI\EventsExtension::TAG_SUBSCRIBER)
-					->addSetup('setTableName', array($config['database']['table']))
-					->addSetup('setColumnNames', array($columns['key'], $columns['locale'], $columns['message'], $columns['updatedAt']));
-			}*/
+
 		}
 
 	}
