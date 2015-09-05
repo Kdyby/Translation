10b711e33d8af7a50743769b4da3843533797f4a
Azathoth <memnarch@seznam.cz>
2015-09-06 00:50:19 +0200

    Added table creation.

--- tests/KdybyTests/data.sql
+++ tests/KdybyTests/data.sql
@@ -1,3 +1,10 @@
+CREATE TABLE `translation` (
+  `key` varchar(50) NOT NULL,
+  `locale` varchar(50) NOT NULL,
+  `message` longtext,
+  `updated_at` datetime NOT NULL
+);
+
 INSERT INTO translation2.translation (`key`, locale, message, updated_at) VALUES ('front.header', 'cs', 'záhlaví', '2015-09-06 00:16:49');
 INSERT INTO translation2.translation (`key`, locale, message, updated_at) VALUES ('front.header', 'en', 'header', '2015-08-30 11:28:17');
 INSERT INTO translation2.translation (`key`, locale, message, updated_at) VALUES ('messages.hello', 'cs', 'nazdar', '2015-09-06 00:16:49');
