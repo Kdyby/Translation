d22a6665278d4845dedb46924a5cc49824f7dbb0
Filip Procházka <filip@prochazka.su>
2017-05-18 18:12:28 +0200

    cleanup tests bootstrap

--- tests/KdybyTests/bootstrap.php
+++ tests/KdybyTests/bootstrap.php
@@ -8,14 +8,13 @@
  * For the full copyright and license information, please view the file license.md that was distributed with this source code.
  */
 
-if (!$autoload = @include __DIR__ . '/../../vendor/autoload.php') {
+if (@!include __DIR__ . '/../../vendor/autoload.php') {
 	echo 'Install Nette Tester using `composer update --dev`';
 	exit(1);
 }
 
 // configure environment
 Tester\Environment::setup();
-class_alias('Tester\Assert', 'Assert');
 date_default_timezone_set('Europe/Prague');
 
 // create temporary directory
